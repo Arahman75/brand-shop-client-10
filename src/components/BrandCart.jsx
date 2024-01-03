6726812d7693c0c60586a1c44bf899bf3a2336a5
Arahman75 <selfarahman01825@gmail.com>
2024-01-03 15:56:14 +0600

    change the firebse hosting

--- src/components/BrandCart.jsx
+++ src/components/BrandCart.jsx
@@ -9,7 +9,8 @@ const BrandCart = ({ brand }) => {
     return (
         <Link to='/products'>
             <div className="card bg-base-100 shadow-xl p-4">
-                <figure><img className='w-3/4 h-[180px]' src={image} alt="brand photo" /></figure>
+                <figure>
+                    <img className='w-3/4 h-[180px]' src={image} alt="brand photo" /></figure>
                 <div className="card-body">
                     <h2 className="card-title justify-center text-2xl">{brandName}</h2>
                 </div>
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,36 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC3yb5bXssIlidwsqYZ0mCrKfva0hUhaMQ",
+//     authDomain: "automotive-brand-321a1.firebaseapp.com",
+//     projectId: "automotive-brand-321a1",
+//     storageBucket: "automotive-brand-321a1.appspot.com",
+//     messagingSenderId: "170881166210",
+//     appId: "1:170881166210:web:2bb3dcc0262c45f2a43d9b"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC3yb5bXssIlidwsqYZ0mCrKfva0hUhaMQ",
-    authDomain: "automotive-brand-321a1.firebaseapp.com",
-    projectId: "automotive-brand-321a1",
-    storageBucket: "automotive-brand-321a1.appspot.com",
-    messagingSenderId: "170881166210",
-    appId: "1:170881166210:web:2bb3dcc0262c45f2a43d9b"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
